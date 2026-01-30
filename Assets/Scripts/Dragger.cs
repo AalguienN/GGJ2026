8f000b54bdc96d9488f87be196aa40b0dc01a6cc
AalguienN <93289662+AalguienN@users.noreply.github.com>
2026-01-30 23:51:44 +0100

    Dragging heh

--- Assets/Scripts/Dragger.cs
+++ Assets/Scripts/Dragger.cs
@@ -1,27 +1,27 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
+using UnityEngine.EventSystems;
 
-public class Dragger : MonoBehaviour
+public class Dragger : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler
 {
-    public Collider coll;
-
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        coll = GetComponent<Collider>();
+
     }
 
-    void Update()
+    public void OnPointerDown(PointerEventData eventData)
     {
-        if (Mouse.current.leftButton.wasPressedThisFrame)
-        {
-            Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
-            RaycastHit hit;
+        throw new System.NotImplementedException();
+    }
 
-            if (coll.Raycast(ray, out hit, 100.0f))
-            {
-                transform.position = ray.GetPoint(100.0f);
-            }
-        }
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
     }
 }
 
