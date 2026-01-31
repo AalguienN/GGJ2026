3fb02dbfaf285c220752e171150aa10abbabdb05
Tovarishch Alcazarovich <62358700+TyElessar@users.noreply.github.com>
2026-01-31 18:30:21 +0100

    .

--- Assets/Scripts/Victor/Game/GameController.cs
+++ Assets/Scripts/Victor/Game/GameController.cs
@@ -5,26 +5,47 @@ public class GameController : MonoBehaviour {
 
   [SerializeField] private RectTransform fillHealthBarTransform_;
   [SerializeField] private float timerMultiplier = 2.0f;  
-  [SerializeField] private EnemyController enemyController_;
+  private float width;
+  private float originalWidth;
 
-  private float width = 540.0f;
+  [SerializeField] private EnemyController enemyController_;
+  [SerializeField] private RectTransform enemyTimeRemaining_;
+  [SerializeField] private float enemyTimeMultiplier_ = 12.0f;
+  [SerializeField] private GameObject enemyTimer_;
+
+  public float accumulatedTime_;
+
 
   void Start() {
-
+    accumulatedTime_ = 0.0f;
+    originalWidth = fillHealthBarTransform_.sizeDelta.x;
+    width = originalWidth;
   }
 
   void Update() {
     MaskTimer();
+    // EnemyTimer();
+
   }
 
-
   void MaskTimer(){
-    if(!enemyController_.gameObject.activeInHierarchy)
-      fillHealthBarTransform_.sizeDelta = new Vector2(width -= Time.deltaTime * timerMultiplier, 20.0f);
+    if(!enemyController_.gameObject.activeInHierarchy){
+      fillHealthBarTransform_.sizeDelta = new Vector2(width - (accumulatedTime_ += Time.deltaTime * timerMultiplier), 20.0f);
+      if(fillHealthBarTransform_.sizeDelta.x <= 0.0f){
+        enemyController_.gameObject.SetActive(true);
+      }
+    }
+  }
 
-    if(fillHealthBarTransform_.sizeDelta.x <= 0.0f )
-      enemyController_.gameObject.SetActive(true);
-
+  void EnemyTimer(){
+    if(enemyController_.gameObject.activeInHierarchy){
+      enemyTimer_.SetActive(true);
+      enemyTimeRemaining_.sizeDelta = new Vector2(width -= Time.deltaTime * enemyTimeMultiplier_, 20.0f);
+      if(enemyTimeRemaining_.sizeDelta.x <= 0.0f){
+        enemyController_.gameObject.SetActive(false);
+        enemyTimeRemaining_.sizeDelta = new Vector2(originalWidth, 0.0f);
+      }
+    }
   }
 
 }
