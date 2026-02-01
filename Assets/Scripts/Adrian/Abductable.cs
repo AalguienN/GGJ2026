f71f57abeb6fae9c420cf92616127d29fe53848a
AalguienN <93289662+AalguienN@users.noreply.github.com>
2026-02-01 12:16:52 +0100

    aaaaaaaaa

---
+++ Assets/Scripts/Adrian/Abductable.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class Abductable : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    ObjetivoSO self;
+
+    void Start()
+    {
+        if (GetComponent<Karen>()) { 
+           self = Resources.Load<ObjetivoSO>("ScriptableObjects/ObjetivoKaren");
+        }
+        else if (GetComponent<Dumb>())
+        {
+            self = Resources.Load<ObjetivoSO>("ScriptableObjects/ObjetivoTonTorres"); 
+        }
+        else if (GetComponent<Druken>())
+        {
+            self = Resources.Load<ObjetivoSO>("ScriptableObjects/ObjetivoKevin"); 
+        }
+        //TODO
+        //else if (GetComponent<M>)
+        //{
+
+        //Resources.Load<ObjetivoSO>("ScriptableObjects/ObjetivoMustang");
+        //}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameController.Instance)
+        {
+            
+        }
+    } 
+    
+    void OnMouseDown()
+    {
+
+    }
+
+    void OnMouseDrag()
+    {
+        
+    }
+
+}
