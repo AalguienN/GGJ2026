5ec7db721c69d057eb1ae2245263ef382e17153f
AalguienN <93289662+AalguienN@users.noreply.github.com>
2026-02-10 23:27:01 +0100

    piu pium mañana mas

---
+++ Assets/Scripts/Adrian/PiumPium.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class PiumPium : MonoBehaviour
+{
+    public Vector3 pos1;
+    public Vector3 pos2;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
